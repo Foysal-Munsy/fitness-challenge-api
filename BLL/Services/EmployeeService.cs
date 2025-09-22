7c5fa59d02c8f16f9610e441355d12c182d5f35e
Foysal-Munsy <bdfoysal555@gmail.com>
2025-09-23 00:20:53 +0600

    [Created] Services Folder and [Added] EmployeeService for core Business Logic.
    1.[Created] a dedicated Services folder for organizing business Logic
    
    2.[added] EmployeeService to encapsulate and manage core logic related to employee data.
    
    3.[set up] basic structure for service layer with methods for CRUD operations and business.

---
+++ BLL/Services/EmployeeService.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class EmployeeService
+    {
+        public List<Employee> Get()
+        {
+            // to get the data from EmployeeRepo
+            // EmployeeRepo Extract the data
+            return EmployeeRepo.Get();
+        }
+
+        //logic implementation for test
+        public List<Employee> Get10()
+        {
+            var data = from e in EmployeeRepo.Get()
+                       where e.Id < 11
+                       select e;
+            return data.ToList();
+        }
+        public Employee Get(int id)
+        {
+            return EmployeeRepo.Get(id);
+        }
+        public bool Create(Employee employee)
+        {
+            return EmployeeRepo.Create(employee);
+        }
+        public bool Update(Employee employee)
+        {
+            return EmployeeRepo.Update(employee);
+        }
+        public bool Delete(int id)
+        {
+            return EmployeeRepo.Delete(id);
+        }
+    }
+}
